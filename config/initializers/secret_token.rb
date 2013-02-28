33351f14518f472a1df5a86845202551f47badf5
Dillon Hafer <dh@dillonhafer.com>
2013-02-28 13:30:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wallbase::Application.config.secret_token = '417d4a445ceadb719f1f7729a88f5d83d03944bf0a77a08a15f081b10b902d3895828da02722f9abc1751b4597a98357f537f62eb515ab987e9a1d1e2be8b2e4'
