efde1f53f089fa0fc53dc243dcdee9e1f0a4696b
Dillon Hafer <dh@dillonhafer.com>
2013-05-20 17:02:51 -0400

    Updated masquerade.

--- app/helpers/color_palettes_helper.rb
+++ app/helpers/color_palettes_helper.rb
@@ -19,6 +19,24 @@ module ColorPalettesHelper
         divs += link_to "Delete", color_path(color), method: :delete, class: 'red', data: {confirm: "Are you sure?"}
         divs += "</div>"
       end
+    elsif color_palette.name.downcase == "wood"            
+      color_palette.colors.order(:color_number).each do |color|
+        divs += "<div class='large-3 columns color-swatch'><div class='swatch shadow' style='background: url(/assets/wood/#{color.color_number}.jpg);'></div><br />
+                       <b>#{color.color_number}</b> #{color.name}<b> #{color.color_foundation}</b> #{in_selects? color}<br />"
+        divs += link_to "Edit", edit_color_path(color)
+        divs += " | "
+        divs += link_to "Delete", color_path(color), method: :delete, class: 'red', data: {confirm: "Are you sure?"}
+        divs += "</div>"
+      end
+    elsif color_palette.name.downcase == "stone"            
+      color_palette.colors.order(:color_number).each do |color|
+        divs += "<div class='large-3 columns color-swatch'><div class='swatch shadow' style='background: url(/assets/stone/#{color.color_number}.jpg);'></div><br />
+                       <b>#{color.color_number}</b> #{color.name}<b> #{color.color_foundation}</b> #{in_selects? color}<br />"
+        divs += link_to "Edit", edit_color_path(color)
+        divs += " | "
+        divs += link_to "Delete", color_path(color), method: :delete, class: 'red', data: {confirm: "Are you sure?"}
+        divs += "</div>"
+      end
     else
       color_palette.colors.order(:color_number).each do |color|
         divs += "<div class='large-3 columns color-swatch'><div class='swatch shadow' style='background: ##{color.hex};'></div><br />
