429c0186300bc44539486cb906168afa84f2d4cf
Dillon Hafer <dh@dillonhafer.com>
2013-05-20 13:56:42 -0400

    Added wallart images and updated admin view.

--- app/helpers/color_palettes_helper.rb
+++ app/helpers/color_palettes_helper.rb
@@ -10,6 +10,15 @@ module ColorPalettesHelper
         divs += link_to "Delete", color_path(color), method: :delete, class: 'red', data: {confirm: "Are you sure?"}
         divs += "</div>"
       end
+    elsif color_palette.name.downcase == "wall art"            
+      color_palette.colors.order(:color_number).each do |color|
+        divs += "<div class='large-3 columns color-swatch'><div class='swatch shadow' style='background: url(/assets/wallart/#{color.color_number}.jpg);'></div><br />
+                       <b>#{color.color_number}</b> #{color.name}<b> #{color.color_foundation}</b> #{in_selects? color}<br />"
+        divs += link_to "Edit", edit_color_path(color)
+        divs += " | "
+        divs += link_to "Delete", color_path(color), method: :delete, class: 'red', data: {confirm: "Are you sure?"}
+        divs += "</div>"
+      end
     else
       color_palette.colors.order(:color_number).each do |color|
         divs += "<div class='large-3 columns color-swatch'><div class='swatch shadow' style='background: ##{color.hex};'></div><br />
