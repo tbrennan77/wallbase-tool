4303d53a81b8a2203898bc4ac80526c8c90f3031
Dillon Hafer <dh@dillonhafer.com>
2013-05-21 13:00:42 -0400

    Fix color number display.

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -24,7 +24,7 @@ module FiltersHelper
       color_palette.colors.each do |color|
       list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;'>               
           <label class='color-label' style='background: url(/assets/wallart/#{color.color_number}.jpg)' data-color='#{color.hex}'>
-            <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', color.color_number)}' value='#{display_color_number color.color_number} #{color.name}' style='display:none'/>
+            <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', color.color_number)}' value='#{color.color_number} #{color.name}' style='display:none'/>
           </label>
         </li>"
       end
@@ -40,7 +40,7 @@ module FiltersHelper
       color_palette.colors.each do |color|
       list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;'>               
           <label class='color-label' style='background: url(/assets/stone/#{color.color_number}.jpg)' data-color='#{color.hex}'>
-            <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', color.color_number)}' value='#{display_color_number color.color_number} #{color.name}' style='display:none'/>
+            <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', color.color_number)}' value='#{color.color_number} #{color.name}' style='display:none'/>
           </label>
         </li>"
       end
