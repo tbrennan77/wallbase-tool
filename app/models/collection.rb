4cbe8513aa3ae1b7170d0b3675b875a4321e9389
Dillon Hafer <dh@dillonhafer.com>
2013-07-22 16:13:22 -0400

    Removed unnecessarry attribute.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,8 @@
 class Collection < ActiveRecord::Base
-  attr_accessible :name, :material, :collection_description, :icon_1,:icon_2,:icon_3,:icon_4, :icon_5,
-    :collection_sections_attributes
+  attr_accessible :name, :material, :collection_description, :icon_1, :icon_2, :icon_3, :icon_4, :icon_5
+  
   validates_presence_of :name, :material
+  
   has_many :style_types, dependent: :destroy
   has_many :profiles, through: :style_types, dependent: :destroy
   has_many :collection_sections, dependent: :destroy
