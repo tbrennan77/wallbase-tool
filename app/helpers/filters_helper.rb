d976f0870d2abdb75a34c4a4da2fd85ffa511a33
Dillon Hafer <dh@dillonhafer.com>
2013-07-22 12:55:26 -0400

    Added color names to titles.

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -36,7 +36,7 @@ module FiltersHelper
     list_items = ""
     if color_palette.name == "Selects"            
       Color.where(selects: true).each do |color|
-      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;''>               
+      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;' class='has-tip' data-tooltip title='#{display_color_number color.color_number} #{color.name} #{color.color_foundation}'>               
             <label class='color-label' style='background: ##{color.hex}' data-color='#{color.hex}'>
               <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', display_color_number(color.color_number))}' value='#{display_color_number color.color_number} #{color.name}' style='display:none'/>
             </label>
@@ -44,7 +44,7 @@ module FiltersHelper
       end
     elsif color_palette.name == "Wall Art"
       color_palette.colors.each do |color|
-      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;'>               
+      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;' class='has-tip' data-tooltip title='#{display_color_number color.color_number} #{color.name} #{color.color_foundation}'>               
           <label class='color-label' style='background: url(/assets/wallart/#{color.color_number}.jpg)' data-color='#{color.hex}'>
             <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', color.color_number)}' value='#{color.color_number} #{color.name}' style='display:none'/>
           </label>
@@ -52,7 +52,7 @@ module FiltersHelper
       end
     elsif color_palette.name == "Wood"
       color_palette.colors.each do |color|
-      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;'>               
+      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;' class='has-tip' data-tooltip title='#{display_color_number color.color_number} #{color.name} #{color.color_foundation}'>               
           <label class='color-label' style='background: url(/assets/wood/#{color.color_number}.jpg)' data-color='#{color.hex}'>
             <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', display_color_number(color.color_number))}' value='#{display_color_number color.color_number} #{color.name}' style='display:none'/>
           </label>
@@ -60,7 +60,7 @@ module FiltersHelper
       end
     elsif color_palette.name == "Stone"
       color_palette.colors.each do |color|
-      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;'>               
+      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;' class='has-tip' data-tooltip title='#{color.color_number} #{color.name} #{color.color_foundation}'>               
           <label class='color-label' style='background: url(/assets/stone/#{color.color_number}.jpg)' data-color='#{color.hex}'>
             <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', color.color_number)}' value='#{color.color_number} #{color.name}' style='display:none'/>
           </label>
@@ -68,7 +68,7 @@ module FiltersHelper
       end
     else
       color_palette.colors.each do |color|
-      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;'>               
+      list_items += "<li style='display:inline-block;margin: 0 8px; width: 50px;' class='has-tip' data-tooltip title='#{display_color_number color.color_number} #{color.name} #{color.color_foundation}'>               
           <label class='color-label' style='background: ##{color.hex}' data-color='#{color.hex}'>
             <input type='checkbox' data-sku='#{@profile.uuid.gsub('XX', display_color_number(color.color_number))}' value='#{display_color_number color.color_number} #{color.name}' style='display:none'/>
           </label>
