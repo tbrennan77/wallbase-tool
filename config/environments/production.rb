9106f7a7dfc7f757fd9ba22a365482a180b1e6b6
Dillon Hafer <dh@dillonhafer.com>
2013-10-02 12:36:25 -0400

    Fix url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Wallbase::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
     :url => 's3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
