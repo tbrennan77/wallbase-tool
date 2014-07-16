80a6e8229dd2aafbcdbacf16847c64ab4505bea8
Dillon Hafer <dh@dillonhafer.com>
2014-07-16 13:14:39 -0400

    Removes sensitive information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def authenticate
-    if Rails.env == "production"        
+    if Rails.env.production?
       authenticate_or_request_with_http_basic do |username, password|      
-        username == 'johnsonite16910' && password == "johnsonite"
+        username == ENV['username'] && password == ENV['password']
       end
     end
   end
