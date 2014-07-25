6b5a3a3730956dba548792e8b3518759e0887551
Dillon Hafer <dh@dillonhafer.com>
2014-07-25 12:36:28 -0400

    Allow xframe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,10 @@ Wallbase::Application.configure do
   if ENV["MEMCACHEDCLOUD_SERVERS"]
     config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
   end
+
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+  }
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
